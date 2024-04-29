cb0897ecd4a68e6519302ac9239f3bbc824dd456
abdellah <abdeallahdz2004@gmail.com>
2024-04-29 16:38:56 +0100

    delete all files

--- frontend/src/redux/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import SetTools from './reducers'
-const rootReducer = combineReducers({
-  // Define your reducers here
-});
-
-export default rootReducer;
--- frontend/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import SetTools from './reducers'
-export const store = configureStore({
-  reducer: {
-    set_tools: SetTools,
-  },
-})
